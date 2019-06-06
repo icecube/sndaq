3e1e5bba7dde80427d96f545d36bf75186adc8c6
Segev BenZvi <sybenzvi@icecube.wisc.edu>
2019-06-06 13:08:38 -0400

    Add package skeleton.

---
+++ python/sndaq/__init__.py
@@ -0,0 +1,11 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+# -*- coding: utf-8 -*-
+"""
+=====
+sndaq
+=====
+IceCube supernova DAQ: search for correlated increases in DOM hits using the
+pDAQ 2 ms data stream.
+"""
+from __future__ import absolute_import
+from ._version import __version__
