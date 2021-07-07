47f859c04c778c2ed1bf59208fbd372d5c0b5868
Spencer Griswold <sgriswold@icecube.wisc.edu>
2021-07-07 17:23:01 -0400

    Added test for buffer get

--- python/sndaq/tests/test_buffer.py
+++ python/sndaq/tests/test_buffer.py
@@ -1,10 +1,14 @@
 import unittest
+import numpy as np
 from sndaq.sndaqbuffer import windowbuffer
 
 
 class TestWindowBuffer(unittest.TestCase):
 
     def test_buffersize(self):
-
         size = 20
         self.assertEquals(windowbuffer(size)._size, size)
+
+    def test_get(self):
+        expected = np.zeros(5160, dtype=np.uint16)
+        self.assertTrue(np.all(windowbuffer(20)[0] == expected))
