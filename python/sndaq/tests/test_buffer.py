d7007814550ae55ac805e58dc92df0cfea26ad9f
Spencer Griswold <sgriswold@icecube.wisc.edu>
2021-07-09 14:48:18 -0400

    Add more tests on buffer

--- python/sndaq/tests/test_buffer.py
+++ python/sndaq/tests/test_buffer.py
@@ -5,10 +5,23 @@ from sndaq.buffer import windowbuffer
 
 class TestWindowBuffer(unittest.TestCase):
 
-    def test_buffersize(self):
+    def test_init(self):
+        buffer = windowbuffer(1)
+        self.assertEqual(buffer._ndom, 5160)
+        self.assertEqual(buffer._mult, 2)
+        self.assertEqual(buffer._idx, 1)
+        self.assertIs(buffer._dtype, np.uint16)
+        self.assertFalse(np.any(buffer.data))
+
+    def test_size(self):
         size = 20
-        self.assertEquals(windowbuffer(size)._size, size)
+        ndom = 40
+        buffer = windowbuffer(size=size, ndom=ndom)
+        self.assertEquals(buffer._size, size)
+        self.assertEquals(buffer.data.size, size*ndom)
 
     def test_get(self):
-        expected = np.zeros(5160, dtype=np.uint16)
-        self.assertTrue(np.all(windowbuffer(20)[0] == expected))
+        # All zeros are expected
+        self.assertFalse(np.any(windowbuffer(20)[0]), "Found non-zero value")
+
+
