15ad6efa6d879e7c86e5e72c9e58f5bbf7cd4a75
Spencer Griswold <sgriswold@icecube.wisc.edu>
2021-08-20 12:03:39 -0400

    Added Processing Code from Development Notebook

--- python/sndaq/analysis.py
+++ python/sndaq/analysis.py
@@ -1,5 +1,7 @@
 import numpy as np
 from sndaq.buffer import windowbuffer
+from sndaq.reader import SN_PayloadReader
+from sndaq.detector import Detector
 
 
 class AnalysisConfig:
@@ -78,6 +80,16 @@ class AnalysisHandler(AnalysisConfig):
         self._rebin_factor = int(self.base_binsize/self.raw_binsize)
         self.buffer_raw = windowbuffer(size=self._size*self._rebin_factor, ndom=ndom, dtype=dtype)
 
+        # variables used for rebinning and staging 2ms data before adding to analysis buffer
+        self._staging_depth = 2000
+        self._staging_buffer = np.zeros((ndom, self._staging_depth), dtype=dtype)
+        self._raw_time = np.zeros(self._staging_depth, dtype=np.uint32)
+        self._raw_dt_utime = int(self.raw_binsize * 1e6)
+        self.payloads_read = np.zeros(5160, dtype=np.uint16)
+        self.current_sndata_file = None
+        self.payload = None
+        self._clock_cycle = int(250 * 2**16)
+
         # Create analyses
         self.analyses = []
         for binning in np.asarray(binnings, dtype=dtype):
@@ -91,11 +103,77 @@ class AnalysisHandler(AnalysisConfig):
         self._accum_count = self._rebin_factor
         self._accum_data = np.zeros(ndom, dtype=dtype)
 
+        # Define detector for reading
+        self.i3 = Detector('./data/full_dom_table.txt')
+
         # Define trigger handler
         # TODO: Move to Alert Handler
         self.trigger_pending = False
         self.trigger_xi = 0.
         self.triggered_analysis = None
+
+    def run(self, n_scalers=1):
+        """Reads n_scalers from each DOM, rebins and writes to analysis buffer"""
+
+        with SN_PayloadReader(self.current_sndata_file) as rdr:
+            self.payload = next(rdr)
+            file_start_time = self.payload.utime
+            self._raw_time = np.arange(file_start_time,
+                                       file_start_time + self._raw_dt_utime * self._staging_depth,
+                                       self._raw_dt_utime)
+            while not self.i3.isvalid_dom(pay.dom_id):
+                pay = next(rdr)
+
+            row = self._process_scalers(pay.utime, pay.scaler_bytes)
+            idx_row = self.i3.get_dom_idx(pay.dom_id)
+            self._staging_buffer[idx_row] += row
+            self.payloads_read[idx_row] += 1
+
+            for i, pay in enumerate(rdr):
+                if self.i3.isvalid_dom(pay.dom_id):
+                    row = self._process_scalers(pay.utime, pay.scaler_bytes)
+                    idx_row = self.i3.get_dom_idx(pay.dom_id)
+                    self._staging_buffer[idx_row] += row
+                    self.payloads_read[idx_row] += 1
+
+                    if pay.utime > self._raw_time[0]:
+                        self.update(self._staging_buffer[:, 0])
+                        self._raw_time = np.roll(self._raw_time, -1)
+                        self._raw_time[-1] = self._raw_time[-2] + self._raw_dt_utime
+                        # Can this rolling operation be done with np.add.at(data[1:]-data[:-1], arange(1, data.size-1)?
+                        self._staging_buffer = np.roll(self._staging_buffer, -1, axis=1)
+                        self._staging_buffer[:, -1] = 0
+
+                if np.all(self.payloads_read >= n_scalers) or np.any(self.payloads_read >= 2):
+                    break
+
+    def _process_scalers(self, utime, scaler_bytes):
+        """time_bins is base (2ms) time bins
+            Could be changed to increment bin time as np.uint16 rather than thru array elements
+        """
+        scalers = np.frombuffer(scaler_bytes, dtype=np.uint8)
+        idx_sclr = scalers.nonzero()[0]
+        raw_counts = np.zeros(self._raw_time.size, dtype=np.uint8)
+        if idx_sclr.size == 0:
+            return raw_counts
+
+        t_sclr = utime + idx_sclr*self._clock_cycle
+        idx_raw = self._raw_time.searchsorted(t_sclr, side="left") - 1
+        np.add.at(raw_counts, idx_raw, scalers[idx_sclr])
+        # Duplicate entries in idx_base (when two 1.6ms bins have bin starts in same 2ms bin) must be added
+        # like so, not via base_counts[idx_base] += scalers[idx_sclr] which only performs addition for first idx
+        # idx_base
+
+        cut = (t_sclr + self._clock_cycle + self._raw_dt_utime > self._raw_time[idx_raw]) & \
+              (t_sclr < self._raw_time[idx_raw] + self._raw_dt_utime)
+        idx_raw = idx_raw[cut]
+        idx_sclr = idx_sclr[cut]
+
+        frac = 1. - ((self._raw_time[idx_raw] + self._raw_dt_utime - t_sclr[cut])/self._clock_cycle)
+        raw_counts[idx_raw] -= np.uint16(0.5+frac*scalers[idx_sclr])
+        raw_counts[idx_raw+1] += np.uint16(0.5+frac*scalers[idx_sclr])
+
+        return raw_counts
 
     @property
     def eps(self):
