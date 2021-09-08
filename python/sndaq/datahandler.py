b2b3887cd7ed6b00d9c2acea3637418d3e440f72
Spencer Griswold <sgriswold@icecube.wisc.edu>
2021-09-08 14:16:35 -0400

    Added data handler docstrings

--- python/sndaq/datahandler.py
+++ python/sndaq/datahandler.py
@@ -1,11 +1,23 @@
+"""Objects and functions for handling and processing raw data used by SNDAQ
+"""
 import numpy as np
 import glob
 from sndaq.reader import SN_PayloadReader
 
 
 class DataHandler:
-
+    """Handler for SN scaler data files
+    """
     def __init__(self, ndom=5160, dtype=np.uint16):
+        """Create DataHandler object
+
+        Parameters
+        ----------
+        ndom : int
+            Number of contributing DOMs
+        dtype
+            Data type for SN scaler arrays
+        """
 
         self._scaler_udt = int(250 * 2**16)
         self._scaler_dt = self.scaler_udt / 1e7
@@ -25,40 +37,116 @@ class DataHandler:
 
     @property
     def files(self):
+        """List of SN scaler file paths
+
+        Returns
+        -------
+        files : list
+            Paths to files containing unprocessed sn scaler data
+        """
         return self._file_glob
 
     def get_data_files(self, directory):
+        """Get SN scaler files from a directory
+
+        Parameters
+        ----------
+        directory : str | os.PathLike
+            Directory to search for SN data files
+        """
         self._file_glob = sorted(glob.glob('/'.join((directory, 'sn*.dat'))))  # May need to check sorting order
 
     def set_file(self, filename):
+        """Set current SN scaler data file
+
+        Parameters
+        ----------
+        filename : str | os.PathLike
+            String or path of SN Scaler data file
+        """
         self._file = SN_PayloadReader(filename)
 
     def read_payload(self):
+        """Read one SN scaler payload from the current file
+
+        See Also
+        --------
+        sndaq.reader.SN_PayloadReader
+        sndaq.reader.SN_Payload
+
+        """
         self._pay = next(self._file)
 
     @property
     def payload(self):
+        """Current SN scaler payload
+
+        Returns
+        -------
+        payload : sndaq.reader.SN_Payload
+            SN scaler payload
+
+        See Also
+        --------
+        sndaq.reader.SN_Payload
+        """
         return self._pay
 
     @property
     def next_scalers(self):  # TODO: Figure out a better name
-        """Return rebinned scalers at the tail of the staging buffer"""
+        """Retrieve the first column of scaler data from staging buffer
+
+        Returns
+        -------
+        scalers : numpy.ndarray
+            ndom-length array on SN scaler data from first column of staging buffer
+        """
         return self._data[:, 0]
 
     def advance_buffer(self):
+        """Roll front of staging buffer off the end, add empty space at back
+        """
         self._raw_utime += self._raw_udt
         # Can this rolling operation be done with np.add.at(data[1:]-data[:-1], arange(1, data.size-1)?
         self._data = np.roll(self._data, -1, axis=1)
         self._data[:, -1] = 0
 
     def update_buffer(self, idx_dom):
+        """Add current payload to staging buffer according to DOM index
+
+        Parameters
+        ----------
+        idx_dom : int
+            Index of staging buffer at which to add the data contained by the current payload
+
+        Notes
+        -----
+        It is assumed that idx_dom corresponds to the current payload contained by _pay
+        """
         data, idx_data = self.rebin_scalers(self._pay.utime, self._pay.scaler_bytes)
         np.add.at(self._data, (idx_dom, idx_data), data)
         self._payloads_read[idx_dom] += 1
 
     def rebin_scalers(self, utime, scaler_bytes):
-        """time_bins is base (2ms) time bins
-            Could be changed to increment bin time as np.uint16 rather than thru array elements
+        """Rebin scalers to 2 ms
+
+        Parameters
+        ----------
+        utime : int
+            Time payload in 0.1 ns since start of year
+        scaler_bytes : bytearray
+            Binary scaler hit data
+
+        Returns
+        -------
+        counts : np.ndarray
+            Scalers restructured in 2 ms bins
+        idx : np.ndarray
+            Indices in which the new scaler counts should be added
+
+        Notes
+        -----
+        Could be changed to increment bin time as np.uint16 rather than thru array elements
         """
         scalers = np.frombuffer(scaler_bytes, dtype=np.uint8)
         idx_sclr = scalers
@@ -88,20 +176,51 @@ class DataHandler:
 
     @property
     def raw_dt(self):
+        """Scaler bin size after rebinning, in ms
+
+        Returns
+        -------
+            Scaler bin size, in units ms, after being processed by rebin_scalers
+
+        See Also
+        --------
+        sndaq.datahandler.rebin_scalers
+        """
         """Return binsize in ms for scalers after rebinning, Default = 2 ms"""
         return self._raw_dt
 
     @property
     def raw_udt(self):
-        """Return binsize in 0.1 ns (units utime) for scalers after rebinning"""
+        """Scaler bin size after rebinning, in 0.1 ns
+
+        Returns
+        -------
+            Scaler bin size, in units 0.1 ns, after being processed by rebin_scalers
+
+        See Also
+        --------
+        sndaq.datahandler.rebin_scalers
+        """
         return self._raw_udt
 
     @property
     def scaler_dt(self):
-        """Return binsize in ms for scalers from sndata file, Default = 1.6384 ms"""
+        """Scaler bin size before rebinning, in ms
+
+        Returns
+        -------
+        binsize : float
+            Scaler bin size, in units ms, from file. Expected = 1.6384 ms"""
         return self.scaler_dt
 
     @property
     def scaler_udt(self):
-        """Return binsize in ms for scalers after rebinning"""
+        """Scaler bin size before rebinning, in 0.1 ns
+
+        Returns
+        -------
+        binsize : int
+            Scaler bin size in units 0.1 ns from file
+        """
         return self._scaler_udt
+
