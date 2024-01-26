642fed5b3645935af5a05d3414faa32ce9922a3e
Segev BenZvi <sybenzvi@gmail.com>
2024-01-25 19:09:10 -0500

    Add support for search extension.

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,9 @@ author = 'Spencer Griswold, Segev BenZvi, IceCube Collaboration'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [ 'sphinx_rtd_theme'
+extensions = [
+    'sphinx_rtd_theme',
+    'sphinx_search.extension',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
