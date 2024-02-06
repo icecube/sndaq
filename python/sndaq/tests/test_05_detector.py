5299d0daac0405a2d05afd45128fa073a48d8209
dahlia <dveyrat@ur.rochester.edu>
2024-02-06 17:08:38 -0500

    Fix file paths in detector module unit tests

--- python/sndaq/tests/test_05_detector.py
+++ python/sndaq/tests/test_05_detector.py
@@ -6,7 +6,7 @@ class TestDetector(unittest.TestCase):
     def test_init(self):
         """Detector initialization
         """
-        detector = Detector()
+        detector = Detector(doms_table='./etc/full_dom_table.txt')
         self.assertEqual(detector._dom_id_map.size, 5160)
         self.assertEqual(detector._dom_id_map[5000], 585)
         self.assertEqual(detector._dom_id_sorted[5000], 272593794653359)
@@ -14,12 +14,12 @@ class TestDetector(unittest.TestCase):
     def test_get_dom_idx(self):
         """Corresponding index
         """
-        detector = Detector()
+        detector = Detector(doms_table='./etc/full_dom_table.txt')
         self.assertEqual(detector.get_dom_idx(272593794653359), 585)
 
     def test_isvalid_dom(self):
         """DOM in SNDAQ SICO analysis
         """
-        detector = Detector()
+        detector = Detector(doms_table='./etc/full_dom_table.txt')
         self.assertFalse(detector.isvalid_dom(1))
         self.assertTrue(detector.isvalid_dom(272593794653359))
