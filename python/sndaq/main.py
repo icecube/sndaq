Fixed units of utime converters

python/sndaq/main.py
@@ -107,7 +107,7 @@ def main(*args, **kwargs):
             dh._file_start_utime = utime
             if dh._start_utime is None and utime is not None:  # First file opening in processing run
                 dh._start_utime = utime
-                ana.set_start_time(utime_to_datetime64(utime, start_time.item().year))
+                ana.set_start_time(utime_to_datetime64(utime, year=start_time.item().year))
                 dh._raw_utime = np.arange(utime, utime + (dh._raw_udt * dh._staging_depth), dh._raw_udt)
 
             stop_utime = stop_time.astype('datetime64[ns]') - np.datetime64(f'{stop_time.item().year}', 'Y')

python/sndaq/util/__init__.py
@@ -12,9 +12,9 @@ def datetime64_to_utime(timestamp):
     Returns
     -------
     utime : int
-        Time since start of year measured in ns
+        Time since start of year measured in 0.1 ns
     """
-    return (timestamp - np.datetime64(f"{timestamp.item().year}", 'Y')).astype('timedelta64[ns]').astype(int)
+    return 10*(timestamp.astype('datetime64[ns]') - timestamp.astype('datetime64[Y]')).astype(int)
 
 
 def utime_to_datetime64(utime, year=np.datetime64('now', 'Y').item().year):
@@ -23,7 +23,7 @@ def utime_to_datetime64(utime, year=np.datetime64('now', 'Y').item().year):
     Parameters
     ----------
     utime : int
-        ns since the start of the year specified by argument `year`
+        0.1 ns since the start of the year specified by argument `year`
     year : int
         Year, if None is provided, the current year is assumed
     Returns
@@ -32,5 +32,5 @@ def utime_to_datetime64(utime, year=np.datetime64('now', 'Y').item().year):
         Time since start of year measured in ns
     """
     if isinstance(utime, (list, tuple, np.ndarray)):
-        return [np.datetime64(f"{year}", 'Y') + np.timedelta64(t, 'ns') for t in utime]
-    return np.datetime64(f"{year}", 'Y') + np.timedelta64(utime, 'ns')
+        return [np.datetime64(f"{year}", 'Y') + np.timedelta64(t//10, 'ns') for t in utime]
+    return np.datetime64(f"{year}", 'Y') + np.timedelta64(utime//10, 'ns')